b42cbdd6df5b3f442ea322d5892ca177fd9f3d61
code-wizard <alexander1990>
2017-07-21 23:31:43 +0530

    added static files

--- account/views.py
+++ account/views.py
@@ -16,12 +16,12 @@ def my_playforms(request):
             user = User.objects.get(pk=request.user.id)
             username = user.username.replace(".", "")
             # subprocess.check_call(["sudo","su", "ebuka" ,"-c","/home/ebuka/test.sh", username+".playboard.xyz",username])
-            # subprocess.check_call(["sudo","/home/ebuka/test.sh", username+".playboard.xyz",username])
+            subprocess.check_call(["sudo","-u","root","/home/ebuka/test.sh", username+".playboard.xyz",username])
             # p = subprocess.Popen(['sudo', '-S',"/home/ebuka/test.sh", username+".playboard.xyz",username],
             #                      stdin=subprocess.PIPE, stderr=subprocess.PIPE,universal_newlines=True)
             # err = p.communicate("alexander" + '\n')[1]
             # print(err)
-            os.popen("sudo -S -u ebuka %s" % ("/home/ebuka/wordpress.sh "+username+".playboard.xyz"+" "+username), 'w').write('alexander1990\n')
+            # os.popen("sudo -S -u ebuka %s" % ("/home/ebuka/wordpress.sh "+username+".playboard.xyz"+" "+username), 'w').write('alexander1990\n')
 
             return render(request,"account/create_sub_domain.html")
         except subprocess.CalledProcessError as e:
