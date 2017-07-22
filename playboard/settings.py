f92cd4c728781cfcfa24b68bd3dd2e2814efe9ad
code-wizard <alexander1990>
2017-07-22 14:22:41 +0530

    added static files

--- playboard/settings.py
+++ playboard/settings.py
@@ -194,4 +194,4 @@ CELERY_ACCEPT_CONTENT=[ 'json', 'msgpack', 'yaml']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 BROKER_URL = 'amqp://guest:guest@localhost:5672//'
-CELERY_RESULT_BACKEND = 'amqp://guest:guest@localhost:5672//'
+CELERY_RESULT_BACKEND = 'amqp://regalix:regalix123@localhost:5672//'
